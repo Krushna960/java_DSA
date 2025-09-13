fa4c7c5aaad198fee3c65223fe2f1b38ba89a1fb
Krushna960 <161699252+Krushna960@users.noreply.github.com>
2025-09-13 16:10:31 +0530

    #

--- 14_Maths/GCD_LCM.java
+++ 14_Maths/GCD_LCM.java
@@ -1,11 +1,15 @@
 public class GCD_LCM {
     public static void main(String[] args) {
-        System.out.println(gcd(4,16));
+        System.out.println(gcd(4,16));//hcm
+        System.out.println(lcm(2,7));//lcm
     }
     static int gcd(int a,int b){
         if(a == 0){
             return b;
         }
         return gcd(b%a, a) ;
+    }
+    static int lcm(int a,int b){
+        return (a*b)/gcd(a,b);
     }
 }
