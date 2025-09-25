01efcdfebbc8d46efbf52c4998398db3f8e6338b
Krushna960 <161699252+Krushna960@users.noreply.github.com>
2025-09-25 15:40:46 +0530

    #

---
+++ 18_Backtracking/AllPaths.java
@@ -0,0 +1,81 @@
+
+import java.util.Arrays;
+
+public class AllPaths {
+    public static void main(String[] args) {
+        boolean[][] maze = {
+            {true,true,true},
+            {true,true,true},
+            {true,true,true}
+        };
+        // allPath("", maze, 0, 0);
+        int[][] path = new int[maze.length][maze[0].length];
+        allPathPrint("", maze, 0, 0, path, 1);
+    }
+    static void allPath(String p,boolean[][] maze, int r, int c){
+        if(r==maze.length-1 && c==maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+        if(!maze[r][c]){
+            return;
+        }
+
+        // i am considering this block in the path
+        maze[r][c] = false;
+
+        if(r<maze.length-1){
+            allPath(p+'D', maze, r+1, c);
+        }
+        if(c<maze[0].length-1){
+            allPath(p+'R', maze, r, c+1);
+        }
+         if(r>0){
+            allPath(p+'U', maze, r-1, c);
+        }
+         if(c>0){
+            allPath(p+'L', maze, r, c-1);
+        }
+
+        //this line is where the function will be over
+        //so before the function gets removed from the stack, and also remove the changes that were made by
+        maze[r][c] = true;
+    }
+    static void allPathPrint(String p,boolean[][] maze, int r, int c, int[][]path, int step){
+
+        if(r==maze.length-1 && c==maze[0].length-1){
+            path[r][c] = step;
+            for(int[] arr : path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+        if(!maze[r][c]){
+            return;
+        }
+
+        // i am considering this block in the path
+        maze[r][c] = false;
+        path[r][c] = step;
+        //print path array
+        if(r<maze.length-1){
+            allPathPrint(p+'D', maze, r+1, c, path, step+1);
+        }
+        if(c<maze[0].length-1){
+            allPathPrint(p+'R', maze, r, c+1, path, step+1);
+        }
+         if(r>0){
+            allPathPrint(p+'U', maze, r-1, c, path, step+1);
+        }
+         if(c>0){
+            allPathPrint(p+'L', maze, r, c-1, path, step+1);
+        }
+
+        //this line is where the function will be over
+        //so before the function gets removed from the stack, and also remove the changes that were made by
+        maze[r][c] = true;
+        path[r][c] = 0;
+    }
+}
