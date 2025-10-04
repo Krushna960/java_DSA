1111ebd14edf5138b3c6ecd3c24f5e3a7f8c9b29
Krushna960 <161699252+Krushna960@users.noreply.github.com>
2025-10-04 19:07:36 +0530

    #

---
+++ 19_OOP/properties/inheritance/BoxWeight.java
@@ -0,0 +1,35 @@
+package properties.inheritance;
+
+public class BoxWeight extends Box {
+    double weight;
+
+
+    public BoxWeight() {
+        this.weight = -1;
+    }
+
+    BoxWeight(BoxWeight other) {
+       super();
+       weight = other.weight;
+    }
+
+    public BoxWeight(double side, double weight) {
+        super(side);
+        this.weight = weight;
+    }
+
+    
+
+    public BoxWeight(double l, double h, double w, double weight){
+        super(l,h,w);//call the parent class constructor
+        //used to initialise values present in parent class
+
+        // System.out.println(super.weight);
+        // System.out.println(this.weight);
+
+        this.weight = weight;
+
+        // super(l,h,w); //error
+    }
+    
+}
---
+++ 19_OOP/properties/inheritance/Main.java
@@ -0,0 +1,16 @@
+package properties.inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        // Box box1 = new Box(4.5,6.7,8.9);
+        // Box box2 = new Box(box1);
+
+        // System.out.println(box1.l+" "+box1.w+" "+box1.h);
+
+        // BoxWeight box3 = new BoxWeight();
+        // BoxWeight box4 = new BoxWeight(2,4,6,8);
+        // System.out.println(box3.h+" "+box3.weight);
+
+        BoxPrice box = new BoxPrice(5,10,200);
+    }
+}
