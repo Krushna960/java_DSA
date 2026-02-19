26f82a4df33551c76a453d966b3139d8ef618916
Krushna960 <161699252+Krushna960@users.noreply.github.com>
2026-02-19 13:04:29 +0530

    #

--- 01_first programme/Primitives.java
+++ 01_first programme/Primitives.java
@@ -1,14 +1,22 @@
 public class Primitives {
     public static void main(String[] args) {
         // Example usage of primitive data types in Java
-        int rollno=64;
-        java.lang.String name="John";
-        float fee=5000.50f;// Using 'f' to denote float literal
-        double percentage=85.5;
-        char grade='A'; 
-        boolean isPassed=true;
-        Long phoneNumber=1234567890L;// Using 'L' to denote long literal
+        int rollno = 64;
+        java.lang.String name = "John";
+        float fee = 5000.50f;// Using 'f' to denote float literal
+        double percentage = 85.5;
+        char grade = 'A'; 
+        boolean isPassed = true;
+        Long phoneNumber = 1234567890L;// Using 'L' to denote long literal
         
+
+        System.out.println("Roll Number: " + rollno);
+        System.out.println("Name: " + name);
+        System.out.println("Fee: " + fee);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Passed: " + isPassed);
+        System.out.println("Phone Number: " + phoneNumber);
         
     }
 }
---
+++ 01_first programme/abcd.java
@@ -0,0 +1,22 @@
+
+public class abcd{
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("enter roll no: ");
+        // int rollno = sc.nextInt();
+        // System.out.println("Roll Number: " + rollno);
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("enter a no: ");
+        // int no = sc.nextInt();
+
+        // if(no % 2 == 0){
+        //     System.out.println("no. is even");
+        // } else{
+        //     System.out.println("no. is odd");
+        // }
+
+
+        
+    }
+}
