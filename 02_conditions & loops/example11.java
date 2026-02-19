53ad1c486b62ef82a2680d7214cbd6e68af18561
Krushna960 <161699252+Krushna960@users.noreply.github.com>
2026-02-19 17:34:56 +0530

    #

---
+++ 02_conditions & loops/example11.java
@@ -0,0 +1,81 @@
+
+import java.util.Scanner;
+
+public class example11 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //CALCULATOR
+
+        // int ans = 0;
+        // while (true) {
+        //     System.out.println("Enter the operator: ");
+        //     char op = sc.next().trim().charAt(0);
+
+        //     if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+        //         int num1 = sc.nextInt();
+        //         int num2 = sc.nextInt();
+
+        //         if (op == '+') {
+        //             ans = num1 + num2;
+        //         }
+
+        //         if (op == '-') {
+        //             ans = num1 - num2;
+        //         }
+
+        //         if (op == '*') {
+        //             ans = num1 * num2;
+        //         }
+
+        //         if (op == '/') {
+        //             ans = num1 / num2;
+        //         }
+
+        //         if (op == '%') {
+        //             ans = num1 % num2;
+        //         }
+
+        //         System.out.println(ans);
+        //     }
+        //     else if (op == 'x' || op == 'X') {
+        //         System.out.println();
+        //     }
+        //     else{
+        //         System.out.println("please enter valid operator: ");
+        //     }
+        //     System.out.println("ans: "+ans);
+        // }
+
+
+
+        //FIBO SERIES
+
+        // int n = sc.nextInt();
+        // int a = 0;
+        // int b = 1;
+        // int count = 2;
+
+        // while(count <= n){
+        //     int temp = b;
+        //     b = a + b;
+        //     a = temp;
+        //     count++;
+        // }
+        // System.out.println(b);
+
+
+        //REVERSE A NUMBER
+    //     System.out.print("enter a number: ");
+    //     int num = sc.nextInt();
+    //     int reversed = 0;
+
+    //     while(num>0){
+    //         int rem = num%10;
+    //         reversed = reversed * 10 + rem;
+    //         num = num/10;
+    //     }
+    //     System.out.println("Reversed number: " + reversed);
+    }
+}
