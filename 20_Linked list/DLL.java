8047aff12b182aadca5195acaf66b71071989fa8
krushna <krushnagarole98@gmail.com>
2026-07-11 22:58:38 +0530

    .

---
+++ 20_Linked list/DLL.java
@@ -0,0 +1,49 @@
+public class DLL {
+
+    private Node head;
+
+    public void insertFirst(int value){
+        Node node = new Node(value);
+        node.next = head;
+        node.prev = null;
+        if(head != null){
+            head.prev = node;
+        }
+        head = node;
+    }
+
+    public void display(){
+        Node node = head;
+        Node last = null;
+        System.out.print("Forward: ");
+        while(node != null){
+            System.out.print(node.value + " -> ");
+            last = node;
+            node = node.next;
+        }
+        System.out.println("END");
+
+        System.out.print("Backward: ");
+        while(last != null){
+            System.out.print(last.value + " -> ");
+            last = last.prev;
+        }
+        System.out.println("START");
+    }
+
+    private class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next, Node prev){
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
--- 20_Linked list/Main.java
+++ 20_Linked list/Main.java
@@ -1,12 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
+        // LL list = new LL();
+        // list.insertFirst(3);
+        // list.insertFirst(2);
+        // list.insertFirst(8);
+        // list.insertFirst(17);
+        // list.insertLast(5);
+        // list.insert(10, 2);
+        // list.display();
+        // System.out.println(list.deleteFirst());
+        // list.display();
+        // System.out.println(list.deleteLast());
+        // list.display();
+        // System.out.println(list.delete(2));
+        // list.display();
+
+        DLL list = new DLL();
         list.insertFirst(3);
         list.insertFirst(2);
         list.insertFirst(8);
         list.insertFirst(17);
-        list.insertLast(5);
-        list.insert(10, 2);
+        
         list.display();
     }
 }
