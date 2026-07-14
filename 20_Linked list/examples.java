af001b5ead9bb6fccf87c0aed8aea1d6b8d21065
krushna <krushnagarole98@gmail.com>
2026-07-14 16:52:08 +0530

    .

---
+++ 20_Linked list/examples.java
@@ -0,0 +1,31 @@
+public class examples {
+    public static void main(String[] args) {
+        
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+                
+
+    public int getDecimalValue(ListNode head) {
+        int sum = 0;
+        while (head != null) {
+            sum = sum * 2 + head.val;
+            head = head.next;
+        }
+        return sum;
+
+    }
+}
