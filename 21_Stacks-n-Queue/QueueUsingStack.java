dc5c483b8c67d28d54c2741074e138e4d0c0eb65
krushna <krushnagarole98@gmail.com>
2026-07-15 15:14:24 +0530

    .

---
+++ 21_Stacks-n-Queue/QueueUsingStack.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+public class QueueUsingStack {
+    private Stack<Integer> first;
+    private Stack<Integer> second;
+
+    public QueueUsingStack() {
+        first = new Stack<>();
+        second = new Stack<>();
+    }
+
+    public void add(int value){
+        first.push(value);
+    }
+
+    public int remove(){
+         while(!first.isEmpty()){
+            second.push(first.pop());
+         }
+         int removed = second.pop();
+         while(!second.isEmpty()){
+            first.push(second.pop());
+         }
+         return removed;
+        
+    }
+
+    public int peek(){
+        while(!first.isEmpty()){
+            second.push(first.pop());
+         }
+         int peeked = second.peek();
+         while(!second.isEmpty()){
+            first.push(second.pop());
+         }
+         return peeked; 
+    }
+
+    public boolean IsEmpty(){
+        return first.isEmpty();
+    }
+}
