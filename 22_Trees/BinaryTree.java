e7a6c901e1dd293af6aa399d44df0c939510ea6c
krushna <krushnagarole98@gmail.com>
2026-07-18 15:57:47 +0530

    .

--- 22_Trees/BinaryTree.java
+++ 22_Trees/BinaryTree.java
@@ -79,4 +79,40 @@ class BinaryTree{
         prettyDisplay(node.left, level + 1);
     }
 
+    public void preOrder(){
+        preOrder(root);
+    }
+    private void preOrder(Node node){
+        if(node == null ){
+            return;
+        }
+        System.out.println(node.value);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder(){
+        preOrder(root);
+    }
+    private void inOrder(Node node){
+        if(node == null ){
+            return;
+        }
+        inOrder(node.left);
+        System.out.println(node.value);
+        inOrder(node.right);
+    }
+
+    public void postOrder(){
+        preOrder(root);
+    }
+    private void postOrder(Node node){
+        if(node == null ){
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.value);
+    }
+
 } 
--- 22_Trees/main.java
+++ 22_Trees/main.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 public class main {
     
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        BinaryTree binaryTree = new BinaryTree();
-        binaryTree.populate(scanner);
-        binaryTree.display();
-        binaryTree.prettyDisplay();
+        // Scanner scanner = new Scanner(System.in);
+        // BinaryTree binaryTree = new BinaryTree();
+        // binaryTree.populate(scanner);
+        // binaryTree.display();
+        // binaryTree.prettyDisplay();
+
+        BST bst = new BST();
+        // int[] nums = { 10, 5, 15, 3, 7, 12, 18 };
+        // bst.populate(nums);
+        // bst.display();
+        // System.out.println("Is the BST balanced? " + bst.balanced());
+
+        int[] sortedNums = { 1, 2, 3, 4, 5, 6, 7 };
+        bst.populateSorted(sortedNums);
+        bst.display();
     }
 }
