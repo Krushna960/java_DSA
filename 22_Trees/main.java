f95d101e3a0b5b710fedab23701c15bb9bc9ba44
krushna <krushnagarole98@gmail.com>
2026-07-19 14:42:40 +0530

    .

--- 22_Trees/main.java
+++ 22_Trees/main.java
@@ -8,14 +8,21 @@ public class main {
         // binaryTree.display();
         // binaryTree.prettyDisplay();
 
-        BST bst = new BST();
+        // BST bst = new BST();
         // int[] nums = { 10, 5, 15, 3, 7, 12, 18 };
         // bst.populate(nums);
         // bst.display();
         // System.out.println("Is the BST balanced? " + bst.balanced());
 
-        int[] sortedNums = { 1, 2, 3, 4, 5, 6, 7 };
-        bst.populateSorted(sortedNums);
-        bst.display();
-    }
+        // int[] sortedNums = { 1, 2, 3, 4, 5, 6, 7 };
+        // bst.populateSorted(sortedNums);
+        // bst.display();
+
+        AVL avl = new AVL();
+        for (int i = 1; i < 10; i++) {
+            avl.insert(i);
+        }
+
+        avl.display();
+        System.out.println("Is the AVL tree balanced? " + avl.balanced());}
 }
