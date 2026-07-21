6fe66e85e86c8656f632928ee32f1358d2fe5f89
krushna <krushnagarole98@gmail.com>
2026-07-21 17:16:53 +0530

    .

---
+++ 22_Trees/segment trees/main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+        // BinaryTree binaryTree = new BinaryTree();
+        // binaryTree.populate(scanner);
+        // binaryTree.display();
+        // binaryTree.prettyDisplay();
+
+        // BST bst = new BST();
+        // int[] nums = { 10, 5, 15, 3, 7, 12, 18 };
+        // bst.populate(nums);
+        // bst.display();
+        // System.out.println("Is the BST balanced? " + bst.balanced());
+
+        // int[] sortedNums = { 1, 2, 3, 4, 5, 6, 7 };
+        // bst.populateSorted(sortedNums);
+        // bst.display();
+
+        // AVL avl = new AVL();
+        // for (int i = 1; i < 10; i++) {
+        //     avl.insert(i);
+        // }
+
+        // avl.display();
+        // System.out.println("Is the AVL tree balanced? " + avl.balanced());}
+
+        int[] arr = {3, 8, 6, 7, -2, -8, 4, 9};
+        SegmentTree tree = new SegmentTree(arr);
+        // tree.display();
+
+        System.out.println(tree.query(1, 6)); // Output: 15
+    }   
+}
